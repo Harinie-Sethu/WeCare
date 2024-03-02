8446e9c1b9be7c28bfcac2dbe3d141cd01c37472
JayMuppidi <jaikarsai.muppidi@gmail.com>
2024-03-02 20:19:15 +0530

    Started working

--- user/health_metrics.tsx
+++ user/health_metrics.tsx
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Linking,Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import {Assets, Image,Button,Colors} from 'react-native-ui-lib';
 
-
-const redirMetrics = (filename: string) => {
+function redirMetrics(filename:string) {
   // Assuming the filenames are valid and exist in the same directory
-  Linking.openURL(filename);
-};
+  window.location.href = filename;
+}
 export default function App() {
   return (
     <View style={styles.container}>
       <Button label={'Transcribe appointment'} size={Button.sizes.medium} backgroundColor={Colors.red30}/>
-      <Button margin-5 label={'Track your health'} size={Button.sizes.medium} backgroundColor={Colors.red30} onPress={() => redirMetrics('health_metrics')}/>
+      <Button margin-5 label={'Track your health'} size={Button.sizes.medium} backgroundColor={Colors.red30} onPress={() => redirMetrics('health_metrics.tsx')}/>
       <Button label={'Sharpen your brain'} size={Button.sizes.medium} backgroundColor={Colors.red30}/>
-      <Text>hoi jay and mansa!</Text>
+      <Text>Helath Metrics hahahah mr.poopy butthead</Text>
       <StatusBar style="auto" />
     </View>
   );
